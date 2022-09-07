61f922301a2723be57612a3928f93f1c49fc6b26
Divyateja Pasupuleti <divyateja2004@gmail.com>
2022-09-07 09:44:11 +0530

    Go back to functional testing

--- tests/message.test.ts
+++ tests/message.test.ts
@@ -3,65 +3,26 @@ import "reflect-metadata";
 import { SendMessage } from './../graphql/mutations/sendMutation';
 import { pubSub } from './../src/schema';
 import { ReceiveMessage } from './../graphql/subscriptions/receiveMessage';
-// import { graphql } from 'graphql';
-import { graphql, GraphQLSchema, parse, subscribe } from "graphql";
-import { createSchema } from "../src/schema";
-
-let schema: GraphQLSchema;
-
+import { testMutation, testSubscription } from "./testKit";
 describe('test the messaging resolver', () => {
     it('tests the messaging service', async () => {
-        if (!schema) schema = await createSchema();
-        // const triggerSubscription = testMutation({
-        //     source: SendMessage,
-        //     variables: {
-        //         message: "Hello!",
-        //         channel: "testChannel"
-        //     }
-        // })
-
-        // const result: any = await testSubscription({
-        //     subscription: ReceiveMessage,
-        //     mutation: triggerSubscription, 
-        //     variablesSubscription: {
-        //         topic: "testChannel"
-        //     }
-        // })
-
-        const triggerSubscription = graphql({
-            schema: schema,
+        const triggerSubscription = testMutation({
             source: SendMessage,
-            variableValues: {
+            variables: {
                 message: "Hello!",
                 channel: "testChannel"
             }
-        });
+        })
 
-        const result: any = await subscribe({
-            schema: schema,
-            document: parse(ReceiveMessage),
-            rootValue: triggerSubscription,
-            variableValues: {
+        const result: any = await testSubscription({
+            subscription: ReceiveMessage,
+            mutation: triggerSubscription, 
+            variablesSubscription: {
                 topic: "testChannel"
             }
-        });
+        })
 
-        expect((await result.next()).value.data).toEqual({
-            UserAdded: {
-              user: {
-                name: 'Awesome Name',
-                username: 'awesomeusername',
-                email: null,
-                isActive: true,
-                followers: {
-                  totalCount: 0,
-                },
-                following: {
-                  totalCount: 0,
-                },
-              },
-            }
-          });
+        console.log(result.next());
           
     })
 })
