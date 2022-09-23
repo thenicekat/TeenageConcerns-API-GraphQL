3f9d4b4c7247c2ed69bcd151015c96344abc94ad
Divyateja Pasupuleti <divyateja2004@gmail.com>
2022-09-23 11:52:27 +0530

    Add Load Balancing

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const PORT = (process.env.PORT_NO && parseInt(process.env.PORT_NO)) || 5000;
+export const PORT = (process.env.PORT && parseInt(process.env.PORT)) || 5000;
 export const COOKIE_KEY = "cookey";
 export const __prod__ = process.env.NODE_ENV === 'production';
 export const __test__ = process.env.NODE_ENV === 'test';
