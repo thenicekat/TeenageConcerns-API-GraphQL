f81b02f2c131b19738fd34ba393320d00dbb55a6
Divyateja Pasupuleti <divyateja2004@gmail.com>
2022-09-26 18:20:01 +0530

    Fix tests

--- tests/gauth.test.ts
+++ tests/gauth.test.ts
@@ -2,6 +2,7 @@ import { testMutation } from './testKit';
 import { GoogleAuthURL } from './../graphql/mutations/gauthURLMutation';
 import { db } from '../src/data-source';
 import { pubSub } from '../src/schema';
+import { GAUTH_REDIRECT_URL } from './../src/constants';
 
 beforeAll(async () => {
     await db.initialize();
@@ -9,7 +10,7 @@ beforeAll(async () => {
 
 describe('test the google auth resolver', () => {
     it('tests if we are getting the correct URL or not', async () => {
-        const URL = "https://accounts.google.com/o/oauth2/v2/auth?access_type=offline&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email%20https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.profile&include_granted_scopes=true&response_type=code&client_id=TESTING_CLIENT_ID&redirect_uri=http%3A%2F%2Flocalhost%3A5000"
+        const URL = "https://accounts.google.com/o/oauth2/v2/auth?access_type=offline&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email%20https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.profile&include_granted_scopes=true&response_type=code&client_id=TESTING_CLIENT_ID&redirect_uri=" + GAUTH_REDIRECT_URL
         const res = await testMutation({
             source: GoogleAuthURL,
         })
