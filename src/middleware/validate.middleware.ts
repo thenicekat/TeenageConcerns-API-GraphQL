213d78b9038de240a2a66c6c52b64b9c4603f9fe
Divyateja Pasupuleti <divyateja2004@gmail.com>
2022-10-08 18:28:27 +0530

    Add Validation for password

--- src/middleware/validate.middleware.ts
+++ src/middleware/validate.middleware.ts
@@ -10,5 +10,8 @@ export const isValidated: MiddlewareFn<ValidateInputType> = ({ args }, next) => 
     if(!args.email.match(/^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$/)){
         throw new Error("Invalid Email Format")
     }
+    if(/^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/.test(args.password)){
+        throw new Error("Check if the password contains Minimum 8 Characters,a Symbol, UpperCase Letter, LowerCase Letter and a number")
+    }
     return next();
 }
