d5ac987fb6c5d3b27ef3378e9a83de7fee341bf9
Divyateja Pasupuleti <divyateja2004@gmail.com>
2022-10-08 18:25:10 +0530

    Remove unnecessary imports in google auth resolver

--- src/resolvers/googleAuth.resolver.ts
+++ src/resolvers/googleAuth.resolver.ts
@@ -3,7 +3,6 @@ import { Context, MentorReturn } from '../types';
 import { Arg, Ctx, Mutation, Resolver } from 'type-graphql';
 import { GAUTH_CLIENT_ID, GAUTH_CLIENT_SECRET, GAUTH_REDIRECT_URL } from '../constants';
 import { Mentor } from '../entity/Mentor';
-import argon2 from "argon2";
 
 const oauth2Client = new google.auth.OAuth2(
   GAUTH_CLIENT_ID,
